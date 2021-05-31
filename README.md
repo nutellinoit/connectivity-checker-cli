07b63ab677593841950985d43965ddd704b341d6
Samuele Chiocca <samuele@sighup.io>
2021-05-31 15:57:27 +0200

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,57 @@
 # connectivity-check-yaml-yq
+
+The goal of this project is to have a simple bash script using:
+
+- yq https://github.com/mikefarah/yq utility to parse YAML files
+- nc
+- timeout
+
+to test network openings from a structured YAML.
+
+This can be useful when checking massive port openings multiple times, for example if 
+your business unit is not in charge of firewall openings.
+
+## How to use
+
+The bundled `checks.yaml` file is an example on the check structure:
+
+```yaml
+common:
+  endpoints:
+    - address: localhost
+      port: 8080
+      protocol: TCP # defaults as TCP check, can be omitted
+    - address: 8.8.8.8
+      port: 53
+      protocol: UDP
+
+environments:
+  dev:
+    endpoints:
+      - address: localhost
+        port: 8081
+        protocol: TCP
+  qa:
+    endpoints:
+      - address: www.google.it
+        port: 80
+        protocol: TCP
+      - address: www.google.it
+        port: 443
+        protocol: TCP
+```
+The map common contains all the endpoints to check every time the script runs.
+The map environments contains all the endpoints specific to a certain environment.
+
+In this example `dev` and `qa` are the parameters you need to pass to the check script.
+At least one environment is mandatory.
+
+The endpoints are an array with elements with:
+
+- `address` address to test
+- `port` the port to check
+- `protocol` the protocol, can be TCP,UDP. If omitted, defaults to TCP.
+
+Example output:
+
+![Example output](./images/example_output.png)
---
+++ connectivity-check.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m'
+YAML_FILE=checks.yaml
+ENVIRONMENT=$1
+
+if [ -z "${ENVIRONMENT}" ]; then
+    echo "Usage ./connectivity-check.sh [environment]"
+    exit 1
+fi
+
+set -eu
+
+function ncCheck() {
+
+
+  EXITCODE=0
+  if [ "$3" == "UDP" ]; then
+    timeout 2 nc -zu -w 1 $1 $2 > /dev/null 2>&1 || EXITCODE=$?
+
+    if [ "$EXITCODE" -eq "0" ]; then
+      echo -e "${GREEN}[SUCCESS]${NC} - $1 is reachable on port $2 with protocol UDP"
+    else
+
+      echo -e "${RED}[FAIL]${NC} - $1 is not reachable on port $2 with protocol UDP"
+    fi
+  else
+    timeout 2 nc -z -w 1 $1 $2 > /dev/null 2>&1 || EXITCODE=$?
+
+    if [ "$EXITCODE" -eq "0" ]; then
+      echo -e "${GREEN}[SUCCESS]${NC} - $1 is reachable on port $2 with protocol TCP"
+    else
+      echo -e "${RED}[FAIL]${NC} - $1 is not reachable on port $2 with protocol TCP"
+
+    fi
+  fi
+
+
+}
+
+ENDPOINTS=$(yq e '.common.endpoints | length' ${YAML_FILE})
+
+for (( c=0; c<${ENDPOINTS}; c++ ))
+do
+  # Test endpoint
+
+  ADDRESS=$(yq e '.common.endpoints['"${c}"'].address' ${YAML_FILE})
+  PORT=$(yq e '.common.endpoints['"${c}"'].port' ${YAML_FILE})
+  PROTOCOL=$(yq e '.common.endpoints['"${c}"'].protocol' ${YAML_FILE})
+
+  ncCheck ${ADDRESS} ${PORT} ${PROTOCOL}
+
+done
+
+ENDPOINTS=$(yq e '.environments.'${ENVIRONMENT}'.endpoints | length' ${YAML_FILE})
+
+for (( c=0; c<${ENDPOINTS}; c++ ))
+do
+  # Test endpoint
+
+  ADDRESS=$(yq e '.environments.'${ENVIRONMENT}'.endpoints['"${c}"'].address' ${YAML_FILE})
+  PORT=$(yq e '.environments.'${ENVIRONMENT}'.endpoints['"${c}"'].port' ${YAML_FILE})
+  PROTOCOL=$(yq e '.environments.'${ENVIRONMENT}'.endpoints['"${c}"'].protocol' ${YAML_FILE})
+
+  ncCheck ${ADDRESS} ${PORT} ${PROTOCOL}
+
+done
